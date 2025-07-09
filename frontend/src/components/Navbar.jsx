1d33a6dbe63f131442d4ee0bd3c3a1112d7a0391
yashubry <yashu.bry04@gmail.com>
2025-07-09 13:20:37 -0400

    added home page and navbar editing more frontend stuff added ticketgen.py thing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import { Box, Flex, Text, Button, Spacer, HStack } from '@chakra-ui/react';
 import { SiAseprite } from "react-icons/si";
 import { ColorModeButton } from "./ui/color-mode"; // Dark mode toggle
 import { useNavigate } from 'react-router-dom';  // <-- you forgot this import!
+import { MdHomeFilled } from "react-icons/md";
+import { LuPartyPopper } from "react-icons/lu";
+
 
 function Navbar() {
   const navigate = useNavigate();
@@ -15,12 +18,34 @@ function Navbar() {
   return (
     <Flex bg="cyan.900" color="white" p="4" alignItems="center" fontStyle="italic">
       <Box p="2">
-        <Text fontSize="3xl" fontWeight="black">TESSERA EVENTS</Text>
+        <Text fontSize="5xl" fontWeight="black">TESSERA</Text>
       </Box>
 
       <Spacer />
 
       <HStack spacing={4}>
+        
+
+        <Button
+          variant="outline"
+          colorScheme="white"
+          leftIcon={<MdHomeFilled />}
+          fontStyle="italic"
+          onClick={() => navigate('/l')}  
+        >
+          <Text color="cyan.200" fontSize="xl" fontWeight="black">home</Text>
+        </Button>
+
+        <Button
+          variant="outline"
+          colorScheme="white"
+          fontStyle="italic"
+          onClick={() => navigate('/events')}  
+          leftIcon={<LuPartyPopper />}
+        >
+          <Text color="cyan.200" fontSize="xl" fontWeight="black">events</Text>
+        </Button>
+
         <Button
           variant="outline"
           colorScheme="white"
@@ -28,15 +53,25 @@ function Navbar() {
           fontStyle="italic"
           onClick={() => navigate('/user/profile')}  // example: navigate to profile
         >
-          Profile
+          <Text color="cyan.200" fontSize="xl" fontWeight="black">profile</Text>
         </Button>
 
         <Button
-          variant="outline"
           colorScheme="white"
-          onClick={handleLogout}                // logout button here
+          color="cyan.100"
+          fontStyle="italic"
+          onClick={() => navigate('/login')}  
         >
-          Log Out
+          <Text color="cyan.100" fontSize="xl" fontWeight="black">log in</Text>
+        </Button>
+
+        <Button
+          colorScheme="white"
+          color="cyan.100"
+          fontStyle="italic"
+          onClick={handleLogout}                // logout button here
+        > 
+          <Text color="cyan.100" fontSize="xl" fontWeight="black">log out</Text>
         </Button>
 
         <ColorModeButton />

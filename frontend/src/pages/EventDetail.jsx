c866eceb4c15626539a7f2c4d3d7db482168ffb3
yashubry <yashu.bry04@gmail.com>
2025-08-12 13:37:23 -0400

    ERROR CODE

--- frontend/src/pages/EventDetail.jsx
+++ frontend/src/pages/EventDetail.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import TesseraSeatPicker from 'tessera-seat-picker';
+
+
+const rows = [
+  [
+    { id: 1, number: 1, tooltip: "$30" },
+    { id: 2, number: 2, tooltip: "$30" },
+    { id: 3, number: 3, isReserved: true, tooltip: "$30" },
+    null,
+    { id: 4, number: 4, tooltip: "$30" },
+    { id: 5, number: 5, tooltip: "$30" },
+    { id: 6, number: 6, tooltip: "$30" }
+  ],
+  [
+    { id: 7, number: 1, isReserved: true, tooltip: "$20" },
+    { id: 8, number: 2, isReserved: true, tooltip: "$20" },
+    { id: 9, number: 3, isReserved: true, tooltip: "$20" },
+    null,
+    { id: 10, number: 4, tooltip: "$20" },
+    { id: 11, number: 5, tooltip: "$20" },
+    { id: 12, number: 6, tooltip: "$20" }
+  ]
+];
 
 function EventDetail() {
   const { id } = useParams(); 
@@ -9,6 +32,17 @@ function EventDetail() {
       <h1>Event Detail Page</h1>
       <p>Showing details for event ID: {id}</p>
       <p>This is a placeholder page</p>
+
+      <TesseraSeatPicker
+      addSeatCallback={addSeatCallback}
+      removeSeatCallback={removeSeatCallback}
+      rows={rows}
+      maxReservableSeats={3}
+      alpha
+      visible
+      loading={loading}
+      />
+
     </div>
   );
 }
